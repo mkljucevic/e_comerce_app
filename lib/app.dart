94187fa429aaea40abe6b07d46c219cc1f2c1db8
Mario K. <mkljucevic06dev@gmail.com>
2024-01-19 19:11:03 +0100

    add onboarding

--- lib/app.dart
+++ lib/app.dart
@@ -1,15 +1,18 @@
+import 'package:ecomerce_app/features/authentication/screens/onboarding/onboarding.dart';
 import 'package:ecomerce_app/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       themeMode: ThemeMode.system,
       theme: TAppTheme.lightTheme,
       darkTheme: TAppTheme.darkTheme,
+      home: const OnBoardingScreen(),
     );
   }
 }
---
+++ lib/features/authentication/screens/onboarding/widgets/onboarding_next_button.dart
@@ -0,0 +1,31 @@
+import 'package:ecomerce_app/features/authentication/controllers/onboarding_controller.dart';
+import 'package:ecomerce_app/utils/constants/colors.dart';
+import 'package:ecomerce_app/utils/constants/sizes.dart';
+import 'package:ecomerce_app/utils/device/device_utility.dart';
+import 'package:ecomerce_app/utils/helpers/helper_functions.dart';
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+
+class OnBoardingNextButton extends StatelessWidget {
+  const OnBoardingNextButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
+
+    return Positioned(
+      right: TSizes.defaultSpace,
+      bottom: TDeviceUtils.getBottomNavigationBarHeight(),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          shape: const CircleBorder(),
+          backgroundColor: dark ? TColors.primary : Colors.black,
+        ),
+        onPressed: () => OnBoardingController.instance.nextPage(),
+        child: const Icon(Iconsax.arrow_right_3),
+      ),
+    );
+  }
+}
