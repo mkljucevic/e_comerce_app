c975644f0f48e35ab654241535f34c7a45df7190
Mario K. <mkljucevic06dev@gmail.com>
2024-01-24 20:12:27 +0100

    add primary header container and custom shape

---
+++ lib/common/widgets/custom_shapes/containers/primary_header_container.dart
@@ -0,0 +1,45 @@
+import 'package:ecomerce_app/common/widgets/custom_shapes/containers/circular_container.dart';
+import 'package:ecomerce_app/common/widgets/custom_shapes/curved_edges/curved_edges_widget.dart';
+import 'package:ecomerce_app/utils/constants/colors.dart';
+import 'package:flutter/material.dart';
+
+class TPrimaryHeaderContainer extends StatelessWidget {
+  const TPrimaryHeaderContainer({
+    super.key,
+    required this.child,
+  });
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return TCurvedEdgeWidget(
+      child: SizedBox(
+        height: 400,
+        child: Container(
+          color: TColors.primary,
+          child: Stack(
+            children: [
+              /// -- Background Custom Shapes
+              Positioned(
+                top: -150,
+                right: -250,
+                child: TCircularContainer(
+                  backgroundColor: TColors.textWhite.withOpacity(0.1),
+                ),
+              ),
+              Positioned(
+                top: 100,
+                right: -300,
+                child: TCircularContainer(
+                  backgroundColor: TColors.textWhite.withOpacity(0.1),
+                ),
+              ),
+              child,
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
