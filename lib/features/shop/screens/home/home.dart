f10e2e7a30947468dd633404232e04ed19ede2d5
Mario K. <mkljucevic06dev@gmail.com>
2024-01-25 08:23:42 +0100

    add custom reusable app bar

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,4 +1,5 @@
 import 'package:ecomerce_app/common/widgets/custom_shapes/containers/primary_header_container.dart';
+import 'package:ecomerce_app/features/shop/screens/home/widgets/home_appbar.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -6,11 +7,17 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
-            TPrimaryHeaderContainer(child: Container()),
+            TPrimaryHeaderContainer(
+              child: Column(
+                children: [
+                  THomeAppBar(),
+                ],
+              ),
+            ),
           ],
         ),
       ),
